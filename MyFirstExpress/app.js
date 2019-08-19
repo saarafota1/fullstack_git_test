d6273fbc79424b4ba78fbe83200fd7ca86100015
Saar Afuta <saar@aplicatzia.com>
2019-08-19 20:48:46 +0300

    async await promise

--- MyFirstExpress/app.js
+++ MyFirstExpress/app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sessionRouter = require('./routes/sessionRoute');
 var moviesRouter = require('./routes/movies');
+var asyncFunctionsRouter = require('./routes/asyncFunctions');
 
 var app = express();
 
@@ -44,6 +45,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/session', sessionRouter);
 app.use('/movies', moviesRouter);
+app.use('/asyncFunctions', asyncFunctionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ MyFirstExpress/routes/asyncFunctions.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var httpResponse = require('./../moduls/httpResponse');
+
+// ************ CALLBACK ***************///
+router.get('/', function (req, res, next) {
+    var response = new httpResponse();
+
+    // code to try and run...
+    fs.readFile('./files/data.js', function (err, data) {
+        if (err) {
+            response.message = err.message;
+            response.success = false;
+            response.error = true;
+            response.data = data;
+            res.json(response);
+            return;
+        }
+        response.success = true;
+        response.data = data;
+        res.json(response);
+    });
+
+
+
+});
+
+
+router.get('/readFileSync', function (req, res, next) {
+    try {
+        var data_from_file = fs.readFileSync('./files/data.js');
+        console.log(data_from_file);
+        res.json(data_from_file);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+// ************** PROMISE **************//
+router.get('/promise', function (req, res, next) {
+    readFromFilePromise('./files/data.js')
+        .then((data) => {
+            console.log(data);
+            return getBookData(data[0].title);
+        })
+        .then((data) => {
+            console.log(data);
+            return getBookImage(data);
+        })
+        .then((data) => {
+            console.log(data);
+            res.json(data);
+        })
+        .catch((err_promise) => {
+            console.log(err_promise);
+            res.json(err_promise);
+        });
+
+    getBookData()
+        .then((data) => {
+
+        })
+        .catch((err) => {
+
+        });
+    console.log("After Promise...");
+});
+
+function getBookData(title) {
+    return new Promise((resolve, reject) => {
+        try {
+            http.get('/getBook', { title: title }, function (err, data) {
+                resolve(data);
+            })
+
+        } catch (err) {
+            // Pass up thrown error to handle with Promise
+            // catch handler
+            reject(err);
+        }
+    });
+}
+
+function readFromFilePromise(url) {
+    var response = new httpResponse();
+    return new Promise((resolve, reject) => {
+        try {
+            var data_from_file = fs.readFileSync('./files/data.json');
+            resolve(data_from_file);
+        } catch (err) {
+            // Pass up thrown error to handle with Promise
+            // catch handler
+            reject(err);
+        }
+    });
+}
+module.exports = router;
+
